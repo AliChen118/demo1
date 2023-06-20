12c63823c5f168642f9ebe30736701786c1e96ca
Ali <1185462216@qq.com>
2023-06-20 08:31:46 +0800

    add f2()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         f1();
+        f2();
     }
 
     public static void f1() {
         System.out.println("f1");
+    }
+
+    public static void f2() {
+        System.out.println("f2");
     }
 }
