829d04b316ac7a90f2da4c7c8916dd1fa35abd12
Ali <1185462216@qq.com>
2023-06-20 08:34:48 +0800

    add f3()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,10 +3,14 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        f1();
+        f3();
     }
 
     public static void f1() {
         System.out.println("f1");
+    }
+
+    public static void f3() {
+        System.out.println("f3");
     }
 }
